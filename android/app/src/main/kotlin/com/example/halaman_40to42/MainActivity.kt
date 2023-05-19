9dcfdeb353eb77edede6d2269fd3ab25e929ea8f
DatumWildan <datumwildan@gmail.com>
2023-05-19 21:09:09 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/halaman_40to42/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.halaman_40to42
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new container());
+}
+
+class container extends StatelessWidget {
+  const container({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: new Scaffold(
+        body: new Container(
+          color: Colors.black,
+          child: new Text('My Container'),
+          height: 300.0,
+          width: 300.0,
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(20.0),
+          foregroundDecoration: new BoxDecoration(color: Colors.lightBlue),
+        ),
+      ),
+    );
+  }
+}
